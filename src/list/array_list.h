fc1e434e6781aae6e47aff0e59fc3f69724b906b
AlanRostem <arostem@gmail.com>
2025-03-26 13:52:25 +0100

    Insertion and append test for ArrayList

--- src/list/array_list.h
+++ src/list/array_list.h
@@ -23,13 +23,14 @@ namespace scr
             :
             m_size(0),
             m_capacity(reservedCapacity),
-            m_elements(std::make_unique<ArrayType>(reservedCapacity))
+            m_elements(new T[reservedCapacity])
         {
 
         }
 
         void Insert(std::size_t index, T value)
         {
+            // TODO: optimize by expanding and shifting to the right in the same operation
             expand();
             shiftRight(index);
             m_elements[index] = value;
@@ -39,30 +40,47 @@ namespace scr
         {
             Insert(m_size, value);
         }
+
+        T Get(std::size_t index)
+        {
+            return m_elements[index];
+        }
+
+        std::size_t Size()
+        {
+            return m_size;
+        }
     private:
         void expand()
         {
+            m_size++;
             if (m_size < m_capacity)
             {
                 return;
             }
+            m_capacity = m_capacity * 3 / 2;
+            auto newElements = std::shared_ptr<T[]>(new T[m_capacity]);
+            for (std::size_t i = 0; i < m_size; i++)
+            {
+                newElements[i] = m_elements[i];
+            }
+            m_elements = newElements;
         }
 
         void shiftRight(std::size_t fromIndex)
         {
-            if (fromIndex == m_size)
+            if (fromIndex == m_size || m_size < 2)
             {
                 return;
             }
-
-            for (std::size_t i = fromIndex; i < m_size; i++)
+            for (std::size_t i = m_size; i >= fromIndex; i--)
             {
-                
+                m_elements[i] = m_elements[i-1];
             }
         }
 
-        std::size_t m_capacity;
         std::size_t m_size;
-        std::unique_ptr<ArrayType> m_elements;
+        std::size_t m_capacity;
+        std::shared_ptr<ArrayType> m_elements;
     };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+#include "list/array_list.h"
+
 int main()
 {
-    std::cout << "Hello, LibScratch!" << std::endl;
+    scr::ArrayList list;
+    list.Append(1);
+    list.Append(2);
+    list.Append(4);
+    list.Append(5);
+    list.Insert(2, 3);
+    for (std::size_t i = 0; i < list.Size(); i++)
+    {
+        std::cout << list.Get(i) << ",";
+    }
+    std::cout << std::endl;
 }
