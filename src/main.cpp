f911773edc83b57d2d6699914932ff6299ea9587
Alan Rostem <42648988+AlanRostem@users.noreply.github.com>
2025-04-09 19:11:44 +0200

    Fix launcher and forgot CPU_Init()

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main()
     const scr::VTableAllocator* heap = scr::system_heap::Get();
     void* cpuVoid = heap->Alloc(sizeof(scr::CPU));
     scr::CPU* cpu = reinterpret_cast<scr::CPU*>(cpuVoid);
+    scr::CPU_Init(cpu);
     scr::CPU_ExecADD_A(cpu, scr::Register::E);
     heap->Free(cpu);
     scr::debug::CheckMemoryLeaks();
